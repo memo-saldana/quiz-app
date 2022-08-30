7d262ec781a615317bc262911e5faf2bc5b5f8ba
Memo Saldana <memo.sal98@gmail.com>
2022-08-30 16:22:55 -0500

    Adds initial quiz template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useEffect, useState, Fragment } from 'react';
+import ProgressBar from './ProgressBar';
+import Question from './Question';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App({questions}) {
+  const [questionIndex, setQuestionIndex] = useState(null)
+  const [answerStatus, setAnswerStatus] = useState(null)
+  const [correctAnswerCount, setCorrectAnswerCount] = useState(0)
+  const [quizComplete, setQuizComplete] = useState(false)
+  
+  useEffect(() => {
+  	setAnswerStatus(null)
+  }, [questionIndex])
+  
+  useEffect(() => {
+  	if (answerStatus) {
+			setCorrectAnswerCount(count => count + 1)
+		}
+  }, [answerStatus])
+
+  const onNextClick = () => {
+  	if (questionIndex === questions.length - 1) {
+    	setQuizComplete(true)
+    } else {
+    	setQuestionIndex(questionIndex == null ? 0 : questionIndex + 1)
+		}
+  }
+  
+  
+  if (questionIndex == null) {
+    return (
+      <div className="quiz">
+        <h1>Start Quiz</h1>
+        <p>This is a simple React quiz.</p><p>Check out the accompanying article over at justacodingblog for a detailed breakdown of how the quiz works!</p>
+        <button className="start" onClick={onNextClick}>Start</button>
+      </div>
+    )
+  }
+	
+	return (
+    <div className="quiz">
+      {quizComplete ? (
+      	<Fragment>
+      	  <h1>Quiz complete!</h1>
+          <p>You answered {correctAnswerCount} questions correctly (out of a total {questions.length} questions)</p>
+        </Fragment>
+      ) : (
+       <Fragment>
+        <ProgressBar currentQuestionIndex={questionIndex} totalQuestionsCount={questions.length} />
+        <Question 
+          question={questions[questionIndex]} 
+          setAnswerStatus={setAnswerStatus}
+        />
+        {answerStatus != null && (
+          <div>
+            <div className="answerStatus">{!!answerStatus ? "Correct! :)" : "Your answer was incorrect :("}</div>
+            <button className="next" onClick={onNextClick}>
+            {questionIndex === questions.length - 1 ? "See results of this quiz" : "Next Question ->"}
+            </button>
+          </div>
+        )}
+      </Fragment>
+      )}
+      
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,43 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const questions = [
+	{
+  	question: "In what contintent is Indonesia?",
+  	answers: ["South America", "Europe", "Asia"],
+  	correctAnswerIndex: 2
+  },
+  {
+  	question: "What is 64 * 3",
+  	answers: [186, 192, 197, 400],
+  	correctAnswerIndex: 1
+  },
+  {
+  	question: "How many faces does a decahedron have?",
+  	answers: [8, 10, 12],
+  	correctAnswerIndex: 1
+  },
+  {
+  	question: "What's the capital city of England?",
+  	answers: ["London", "Paris", "Berlin"],
+  	correctAnswerIndex: 0
+  },
+  {
+  	question: "Is JavaScript the same as Java?",
+  	answers: ["Yes, it is", "No, it isn't"],
+  	correctAnswerIndex: 1
+  },
+  {
+  	question: "What month comes after November?",
+  	answers: ["January", "March", "December"],
+  	correctAnswerIndex: 2
+  }
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App questions={questions}/>
   </React.StrictMode>
 );
 
