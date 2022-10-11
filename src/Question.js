afb6e52154ccfc40acfc250faeb9e445fc128f95
Memo Saldana <memo.sal98@gmail.com>
2022-10-11 03:13:39 -0500

    Removes correct answer styles and allows changing answer before next click

--- src/Question.js
+++ src/Question.js
@@ -13,30 +13,12 @@ const Question = ({ question, setAnswerStatus }) => {
   	setSelectedAnswerIndex(null)
   }, [question])
   
-  const getClasses = (index) => {
-  	let classes = []
-    if (selectedAnswerIndex != null) {
-      if (selectedAnswerIndex === index) {
-        classes.push("selected")
-      }
-      if (index === question.correctAnswerIndex) {
-        if (selectedAnswerIndex === index) {
-          classes.push("correct")
-        } else {
-          classes.push("incorrect")
-        }
-      }
-    }
-    
-    return classes.join(" ")
-  }
-  
 	return (
   	<div className="question">
       <div className="questionText">{question.question}</div>
       <div className="answers">
         {question.answers.map((answer, index) => {
-        	return <div key={index} className={`answer ${getClasses(index)}`} onClick={() => selectedAnswerIndex == null && setSelectedAnswerIndex(index)}>{answer}</div>
+        	return <div key={index} className={selectedAnswerIndex === index? 'answer selected': 'answer'} onClick={() => setSelectedAnswerIndex(index)}>{answer}</div>
       	})}
       </div>
     </div>
